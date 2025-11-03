d1ddfcf92f78efacb22a8c81a5af861c01689dae
Lian <gj39981225@163.com>
2025-11-03 20:09:24 +0800

    feat(etp): 现在的报错信息也支持彩色啦！
    - 其实目前只有大红色

--- mylib/etp/etp.py
+++ mylib/etp/etp.py
@@ -1,7 +1,14 @@
-class ConfigError(Exception):
+class RedError(Exception):
+    """带红色输出的基础错误"""
+    def __str__(self):
+        return f"\033[31m{super().__str__()}\033[0m"
+
+
+class ConfigError(RedError):
     """配置文件加载失败"""
     pass
 
-class EnvError(Exception):
+
+class EnvError(RedError):
     """环境变量文件加载失败"""
     pass
